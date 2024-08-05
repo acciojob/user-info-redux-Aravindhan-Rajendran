57f43964f1caa4cfac990504cbf7b1e72181d7be
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-05 23:18:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-
-import React from "react";
-import './../styles/App.css';
+// App.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setName, setEmail } from './userSlice';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const name = useSelector((state) => state.user.name);
+  const email = useSelector((state) => state.user.email);
 
-export default App
+  const handleNameChange = (e) => {
+    dispatch(setName(e.target.value));
+  };
+
+  const handleEmailChange = (e) => {
+    dispatch(setEmail(e.target.value));
+  };
+
+  return (
+    <div className="app">
+      <h1>User Info</h1>
+      <div>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={handleNameChange} placeholder="Enter name" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} placeholder="Enter email" />
+        </label>
+      </div>
+      <div>
+        <h2>Current values in store:</h2>
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
