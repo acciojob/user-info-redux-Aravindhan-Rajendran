67ad4f9971ffe0857ffb4ba977f8ef7acaac07e4
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 00:37:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
 import App from './App';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
